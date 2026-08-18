e2950fa3babcb43ae72ed0de281ea9725608bdee
MoProMax <benmomoben321@gmail.com>
2026-08-18 22:21:29 +0200

    chore(next): allow image.thum.io in next.config for optimized remote images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.thum.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
